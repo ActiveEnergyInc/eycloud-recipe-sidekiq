95b48816d2a2fa4b3348314e67da12e0572c6fcf
Zaid Al-Jarrah <zaid.aljarrah@activeenergy.ca>
2013-03-05 16:19:35 -0500

    Only restart apps that use Sidekiq

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -4,6 +4,10 @@ if sidekiq_instance?
   end
 
   node[:applications].each do |app_name, data|
+
+    # Only restart applications that use Sidekiq.
+    next unless node[:sidekiq][:applications].include?(app_name)
+
     execute "restart-sidekiq" do 
       command %Q{ 
         echo "sleep 20 && monit -g #{app_name}_sidekiq restart all" | at now 
